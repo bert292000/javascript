acaca1c146bf68faa661979cc27d5ed55f6b80c9
bert292000 <bert292000@yahoo.com>
2022-09-18 22:19:03 -0600

    Javascript

---
+++ app.js
@@ -0,0 +1,18 @@
+let name = 'Robert';
+let age = 41;
+const dob = 'october 23, 1981';
+let color = 'Blue'
+console.log(name + ' is ' + age + ' years old, he was born on ' + dob + ' and his favourite color is ' + color);
+
+let wife = 'Cheryl';
+let agew = 42;
+const dobw = 'May 29, 1980';
+let colorw = 'Purple';
+console.log(wife + ' is '+ agew +' years old, she was born on '+dobw + ' and her favourite color is ' + colorw);
+let total = age+agew;
+let less = agew-age
+if(age < agew){
+   console.log(wife + ' is older than '+ name +' by ' + less+ ' year')
+}else
+console.log(name + ' is older than '+ wife +' by ' + less)
+console.log('The sum of their age is '+ total)
