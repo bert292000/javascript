9c1e79d84ca848471d7509409b7d1560d08fc660
bert292000 <bert292000@yahoo.com>
2022-09-19 21:49:21 -0600

    app2 updated

--- app2.js
+++ app2.js
@@ -1,8 +1,10 @@
 for(let counter=0; counter <= 100; counter++){
     if(counter % 2 == 0 && counter % 3 == 0){
-            console.log('poobazz'); 
- } else if (counter % 2 == 0){
-            console.log('poo')
- }else 
-            console.log('bazz')
+            console.log('poobazz '); 
+ } else if (counter % 2 == 0){ + counter
+            console.log('poo ' + counter)
+ }else if (counter % 3 == 0){
+            console.log('bazz ' + counter)
+}else
+    console.log (counter)
 }
